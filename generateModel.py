12d44edc6be0b9523653abb0aeeb7dcbe90e5a6e
tnc1 <tnc1@sfu.ca>
2021-07-20 16:34:23 -0700

    removed folder

--- generateModel.py
+++ generateModel.py
@@ -121,20 +121,3 @@ model.fit(
 )
 
 model.save('saved_model/my_model.h5')
-
-sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
-sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
-
-img = keras.preprocessing.image.load_img(
-    sunflower_path, target_size=(img_height, img_width)
-)
-img_array = keras.preprocessing.image.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # Create a batch
-
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
-
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
